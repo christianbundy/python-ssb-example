792553e7eabb46028a6d2b83106490b593c2f3f4
Christian Bundy <christianbundy@fraction.io>
2020-05-24 15:07:34 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import base64
+import json
+import nacl.encoding
+import nacl.signing
+import time
+
+signing_key = nacl.signing.SigningKey.generate()
+
+def get_author(signing_key):
+    key_bytes = bytes(signing_key.verify_key)
+    key_base64 = base64.encodebytes(key_bytes).strip().decode()
+    return '@' + key_base64 + '.ed25519'
+
+def get_signature(signing_key, unsigned_value):
+    unsigned_json = json.dumps(unsigned_value, indent=2)
+
+    signature_bytes = signing_key.sign(bytes(unsigned_json, 'utf8')).signature
+    signature_base64 = base64.encodebytes(signature_bytes).strip().decode()
+    return signature_base64 + '.sig.ed25519'
+
+value = {}
+value['previous'] = None
+value['author'] = get_author(signing_key)
+value['sequence'] = 1
+value['timestamp'] = round(time.time() * 1000)
+value['hash'] = 'sha256'
+value['content'] = {}
+value['content']['type'] = 'post'
+value['content']['text'] = 'hello world'
+value['signature'] = get_signature(signing_key, value)
+
+print(json.dumps(value, indent=2))
